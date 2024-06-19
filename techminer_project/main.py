8299b80033a59b5ee61d4cc2ece255918ab39493
Mario Nelson <marionelson616@gmail.com>
2024-06-18 21:52:43 -0400

    Added the functionality to extract most news headlines from the techcrunch main page

--- techminer_project/main.py
+++ techminer_project/main.py
@@ -1,13 +1,10 @@
 import requests
-from bs4 import BeautifulSoup
+from code_utils.utils import *
+
 url = "https://techcrunch.com/"
 
+# Making the HTTP request to the requested url
 response = requests.get(url)
 
-if response.status_code == 200:
-    html_content = response.content
-    print("Successful request")
-    soup = BeautifulSoup(html_content, 'lxml')
-    print(soup.prettify())
-else:
-    print("Request failed")
+# Function to check the server response, if successful the script will extract the desired data
+validate_extract(response)
