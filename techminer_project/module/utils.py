a11c51f6605f43fd3924668c98f90aa6ddd41461
Mario Nelson <marionelson616@gmail.com>
2024-06-20 23:27:05 -0400

    News data extraction function completed

--- techminer_project/module/utils.py
+++ techminer_project/module/utils.py
@@ -1,15 +1,25 @@
 from bs4 import BeautifulSoup
 
+# Check the server response
+
 def validate_response(response_html):
     return response_html.status_code == 200
+
+# Extract the HTML from the URL
 
 def extract_data(html_content):
     soup = BeautifulSoup(html_content, 'lxml')
     
-    extract_titles(soup)
+    extract_news(soup)
 
-def extract_titles(soup):
-    elements = soup.select("article h2, div.wp-block-tc23-post-picker h2, h2.has-link-color wp-block-post-title wp-elements-bfa04303c7f1029d1f026872d399c7ae")
+# Extract the selected data such as news titles and links
+
+def extract_news(soup):
+    elements = soup.select("div.wp-block-tc23-post-picker h2 a")
+
     for element in elements:
         title = element.get_text().strip()
+
+        link = element["href"]
         print(f"Title: {title}")
+        print(f"Link: {link}")
